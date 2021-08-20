ed6a72b5f8b05303caa73066ccd49f275470718f
Diego Quintero <d86webs@gmail.com>
2021-08-19 22:59:48 -0300

    Pre entrega proyecto final

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -4,6 +4,7 @@ import NavBar from "./../Components/NavBar";
 import ItemListContainer from './../Components/ItemListContainer';
 import ItemDetailContainer from './../Components/ItemDetailContainer';
 import Categorias from '../Components/Categorias';
+import Cart from './../Components/Cart';
 import NotFound from './../Components/NotFound';
 
 const Router = () => {
@@ -15,6 +16,7 @@ const Router = () => {
                     <Route exact path='/' component={ItemListContainer} />
                     <Route path='/Categorias/:idCategoria' component={Categorias} />
                     <Route path='/Detalle/:idItem' component={ItemDetailContainer} />
+                    <Route path='/Carrito' component={Cart} />
                     <Route path='*' component={NotFound} />
                 </Switch>
             </BrowserRouter>
