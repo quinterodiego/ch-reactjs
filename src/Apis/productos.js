6ced333dd8aa40e11a80d2f7df0c4195b64f1117
Diego Quintero <d86webs@gmail.com>
2021-08-19 21:38:51 -0300

    agrego imgs

--- src/Apis/productos.js
+++ src/Apis/productos.js
@@ -3,28 +3,28 @@
 const productos = [
     {
         id: 1,
-        title: "Producto 1",
+        title: "Q1",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
-        pictureUrl: "https://via.placeholder.com/150"
+        pictureUrl: "./../img/Q1/q1-01.jpg"
     },
     {
         id: 2,
-        title: "Producto 2",
+        title: "K2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
         pictureUrl: "https://via.placeholder.com/150"
     },
     {
         id: 3,
-        title: "Producto 3",
+        title: "K4",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
         pictureUrl: "https://via.placeholder.com/150"
     },
     {
         id: 4,
-        title: "Producto 4",
+        title: "K6",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
         pictureUrl: "https://via.placeholder.com/150"
