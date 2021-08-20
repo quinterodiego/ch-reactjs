1b96a031849b6e84aff5d3356ed81101fc6f7bb8
Diego Quintero <d86webs@gmail.com>
2021-08-19 21:49:33 -0300

    arreglo ruta img

--- src/Apis/productos.js
+++ src/Apis/productos.js
@@ -6,28 +6,28 @@ const productos = [
         title: "Q1",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
-        pictureUrl: "./../img/Q1/q1-01.jpg"
+        pictureUrl: "img/Q1/q1-01.jpg"
     },
     {
         id: 2,
         title: "K2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
-        pictureUrl: "https://via.placeholder.com/150"
+        pictureUrl: "img/K2/k2-01.jpg"
     },
     {
         id: 3,
         title: "K4",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
-        pictureUrl: "https://via.placeholder.com/150"
+        pictureUrl: "img/K4/k4-01.jpg"
     },
     {
         id: 4,
         title: "K6",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula.",
         price: 10000,
-        pictureUrl: "https://via.placeholder.com/150"
+        pictureUrl: "img/K6/k6-01.jpg"
     }
 ]
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <>
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand text-white" href="/">Mi Proyecto</a>
+                <a className="navbar-brand text-white" href="/">DO SAN</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
